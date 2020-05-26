351e4e83bb21bbccd30a71b53aef04ef2fc4cbd9
Ciryandil <soham.ganguly12@gmail.com>
2020-05-26 14:21:36 +0530

    US Election Predictor. v1. Need more features.

---
+++ predictor.m
@@ -0,0 +1,29 @@
+%Initialization
+%
+%
+clear;close all;clc;
+data = load('econdata.txt');
+X = data(:,[1,2,3,4]);
+y = data(:,5);
+[m,n] = size(X);
+X = [ones(m,1) X];
+initial_theta = zeros(n+1,1);
+%
+%
+%Training
+%
+%
+lambda=1;
+[cost, grad] = costFunction(initial_theta, X, y,lambda);
+options = optimset('GradObj','on','MaxIter','500');
+[theta,cost] = ...
+    fminunc(@(t)(costFunction(t,X,y,lambda)),initial_theta,options);
+%
+%
+%Predicting
+res=load('hypodata.txt');
+[l,b]=size(res);
+res=[ones(l,1) res];
+p = predict(theta,res);
+
+%
